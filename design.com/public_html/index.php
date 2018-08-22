304c1cebf0f0883849d605b9d9b1b0be4c051dc5
anjanchakraborty <anjankrishnachakaraborty@gmail.com>
2018-08-22 13:18:05 +0530

    design added

---
+++ design.com/public_html/index.php
@@ -0,0 +1,293 @@
+<html>
+<head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+  <title>Dazzle</title>
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="design_files/bootstrap.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Optional Bootstrap theme -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="design_files/bootstrap-theme.css">
+  <script src="design_files/jquery.js"></script>
+  
+  <script src="design_files/bootstrap.js"></script>
+  
+ <link rel="stylesheet" type="text/css" href="css/style.css"> 
+</head>
+
+
+
+<body>
+<!-- //////////////////////////////////////HEADER/////////////////////////////////////////////////////////////////// -->
+<header id="header">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-2">
+        <div class="logo">
+          <img src="design_files/dazzle1.png" alt="img">
+        </div>
+      </div>
+      <div class="col-sm-10">
+        <nav id="main-navigation">
+          <ul class="topnav">
+            <li><a href="#HOME" class="active">HOME</a></li>
+            <li><a href="#STORE">STORE</a></li>
+            <li><a href="#ABOUTUS">ABOUT US</a></li>
+            <li class="dropdown">
+              <a class="dropdown-toggle" data-toggle="dropdown" href="#">PAGES
+              <span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">HTML</a></li>
+                <li><a href="#">CSS</a></li>
+                <li><a href="#">JAVA</a></li>
+              </ul>
+            </li>
+            <li><a href="#BLOG">BLOG</a></li>
+            <li><a href="#CONTACTS">CONTACTS</a></li>  
+          </ul>
+        </nav>
+      </div>
+      <span id="resposive-menu-toggle">&#9776;</span>
+    </div>
+  </div>
+</header>
+
+  
+
+<!-- CONTENT //////////////////////////////////////////////////////////////////////////////////////////////////////-->
+
+<div class="wrapper">
+  <div class="equal">
+    <div class="col-lg-3 col-sm-6 col-xs-12 padding-0">
+      <div class="block  image" >
+        <img src="design_files/blog-thumb7.jpg" class="img-responsive">
+      </div>
+    </div>
+    
+    <div class="col-lg-3 col-sm-6 col-xs-12 padding-0">
+      <div class="block text">
+      <h2>
+        Top Summer Jewelry Trends 2016
+      </h2>  
+      <p>
+           Today we will talk about the most popular jewelry
+            trends of this summer that were 
+              
+
+      </p>
+
+              
+         <ul class="icons">
+            <li><i class="fa fa-clock-o ic"></i>01.11.2017</li>
+            <li><i class="glyphicon glyphicon-folder-open"></i>RECENT</li>
+            <li><i class="fa fa-eye ic"></i>HITS:21</li> 
+          </ul>
+      
+      <div>
+           <!-- Trigger the modal with a button -->
+  <a data-toggle="modal" data-target="#myModal1"> READ MORE<b class="fa fa-chevron-right"></b></a>
+
+  <!-- Modal -->
+  <div class="modal fade" id="myModal1" role="dialog">
+    <div class="modal-dialog">
+    
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h2 class="modal-title">Top Summer Jewelry Trends 2016</h2>
+        </div>
+        <div class="modal-body">
+          <img src="design_files/blog-thumb7.jpg" class="image"><p>Today we will talk about the most popular jewelry
+            trends of this summer that were</p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+      </div>
+    </div>
+  </div>
+      </div>
+    </div>
+    <div class="col-lg-3 col-sm-6 col-xs-12 padding-0">
+      <div class="block image" >
+      <img src="design_files/blog-thumb6.jpg" class="img-responsive">
+    </div>
+    </div>
+        <div class="col-lg-3 col-sm-6 col-xs-12 padding-0">
+          <div class="block text">
+          <h2>
+            Choosing Wedding jewelry:Top5 Rules
+          </h2>
+            <p> 
+               
+                 It may seem tricky to pick proper wedding jewelry
+                 so that it could make you shine...
+            </p> 
+               <ul class="icons">
+                <li><i class="fa fa-clock-o ic"></i>01.11.2017</li>
+                <li><i class="glyphicon glyphicon-folder-open"></i>LATEST</li>
+                <li><i class="fa fa-eye ic"></i>HITS:23</li> 
+               </ul>
+            
+           <div>
+                 <!-- Trigger the modal with a button -->
+  <a data-toggle="modal" data-target="#myModal1"> READ MORE<b class="fa fa-chevron-right"></b></a>
+
+  <!-- Modal -->
+  <div class="modal fade" id="myModal2" role="dialog">
+    <div class="modal-dialog">
+    
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h2 class="modal-title">Choosing Wedding jewelry:Top5 Rules </h2>
+        </div>
+        <div class="modal-body">
+          <img src="design_files/blog-thumb6.jpg" class="image"><p>It may seem tricky to pick proper wedding jewelry so that it could make you shine... </p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+      
+    </div>
+  </div>
+           </div>
+          </div>
+   </div>
+   </div>
+
+  <div class="equal">   
+    <div class="col-lg-3 col-sm-6 col-xs-12 padding-0">
+     <div class="block text">
+      <h2>
+       How to Wear jewelry According to Fashion
+      </h2>
+      <p>
+          Whether fancy or costume,nothing can add more wardrobe like jewelry.Here are...
+              
+      </p>
+               <ul class="icons">
+                <li><i class="fa fa-clock-o ic"></i>01.11.2017</li>
+                <li><i class="glyphicon glyphicon-folder-open"></i>INFORMATION</li>
+                <li><i class="fa fa-eye ic"></i>HITS:368</li> 
+               </ul>
+      
+     <div>
+        <!-- Trigger the modal with a button -->
+  <a data-toggle="modal" data-target="#myModal1"> READ MORE<b class="fa fa-chevron-right"></b></a>
+
+  <!-- Modal -->
+  <div class="modal fade" id="myModal3" role="dialog">
+    <div class="modal-dialog">
+    
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h2 class="modal-title">How to Wear jewelry According to Fashion</h2>
+        </div>
+        <div class="modal-body">
+          <img src="design_files/blog-thumb1.jpg" class="image"><p>Whether fancy or costume,nothing can add more wardrobe like jewelry.Here are... 
+         </p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+      
+    </div>
+  </div>
+     </div>
+    </div>
+    </div>
+     
+
+
+     <div class="col-lg-3 col-sm-6 col-xs-12 padding-0">
+      <div class="block image1">
+      <img src="design_files/blog-thumb1.jpg" class="img-responsive">
+     </div>
+     </div>
+
+
+
+     <div class="col-lg-3 col-sm-6 col-xs-12 padding-0">
+      <div class="block text">
+       <h2>
+        Beloved Silver Jewelry Gets an Update
+       </h2>
+       <p>  
+          The Los Angeles-based designer makes her first
+          foray into fine jewewlry,unveiling a...
+ 
+       </p> 
+               <ul class="icons">
+                <li><i class="fa fa-clock-o ic"></i>01.11.2017</li>
+                <li><i class="glyphicon glyphicon-folder-open"></i>NEWS</li>
+                <li><i class="fa fa-eye ic"></i>HITS:25</li> 
+               </ul>       
+       
+       <div>
+        <!-- Trigger the modal with a button -->
+  <a data-toggle="modal" data-target="#myModal1"> READ MORE<b class="fa fa-chevron-right"></b></a>
+
+  <!-- Modal -->
+  <div class="modal fade" id="myModal4" role="dialog">
+    <div class="modal-dialog">
+    
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h2 class="modal-title">Beloved Silver Jewelry Gets an Update</h2>
+        </div>
+        <div class="modal-body">
+          <img src="design_files/blog-thumb2.jpg" class="image"><p>The Los Angeles-based designer makes her first foray into fine jewewlry,unveiling a...  
+          </p> 
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+      
+    </div>
+  </div>
+
+
+      </div>
+   </div>
+   </div>
+
+
+   <div class="col-lg-3 col-sm-6 col-xs-12 padding-0">
+    <div class="block image1">
+   <img src="design_files/blog-thumb2.jpg" class="img-responsive">
+  </div>
+  </div>
+  </div>
+</div>
+<!-- JAVASCRIPT //////////////////////////////////////////////////////////////////////////////////////////////////////-->
+
+
+  <script>
+/*function openNav() {
+    document.getElementById("myNav").style.display = "block";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.display = "none";
+}
+*/
+$("#resposive-menu-toggle").click(function(){
+  $("#main-navigation").fadeToggle();
+});
+
+</script>
+
+</body>
+</html>
